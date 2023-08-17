d73fb9eec30c21c0c95eb7e9cb94d4ff690413ca
theduykh <theduykh@gmail.com>
2023-08-17 14:59:31 +0700

    set thread

--- src/main/java/io/theduykh/ata/listeners/AtaSuiteListener.java
+++ src/main/java/io/theduykh/ata/listeners/AtaSuiteListener.java
@@ -1,12 +1,13 @@
 package io.theduykh.ata.listeners;
 
+import io.theduykh.ata.utils.ConfigReader;
 import org.testng.ISuite;
 import org.testng.ISuiteListener;
 
 public class AtaSuiteListener implements ISuiteListener {
     @Override
     public void onStart(ISuite suite) {
-        String threads = System.getProperty("threads");
+        String threads = ConfigReader.getString("thread");
         if (threads == null || threads.isEmpty()) {
             suite.getXmlSuite().setDataProviderThreadCount(1);
         } else {
